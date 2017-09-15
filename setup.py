b90f4b6ad69ea07fb8094f27bba1d7384d32ff65
Charlie Root <root@kanazuchi.freebsd-do>
2017-09-15 14:23:09 +0000

    some changes

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="wmi_query",
     version="0.1",
+    license="BSD",
     packages=find_packages(),
+    keywords="wmi wmic",
+    install_requires=['wmic'],
     author='Silvio AS a.k.a kanazuchi',
     author_email='contato@kanazuchi.com',
     scripts=['scripts/wmi_query_cmd'],
