cc2a007d16f04098eef5d36584dc7cbcce9c287a
kanazux <alvolivre@live.com>
2017-09-19 09:10:09 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="py-wmi-query",
     version="0.1",
-    license="BSD",
+    license="BSD2CLAUSE",
     packages=find_packages(),
     keywords="wmi impacket",
-    url="https://github.com/kanazux/wmi_query",
+    url="https://github.com/kanazux/py-wmi-query",
     author='Silvio AS a.k.a kanazuchi',
     author_email='contato@kanazuchi.com',
     scripts=['scripts/wmi-query'],
-    description="A simple way to convert wmi data from wmic into dict objects.",
+    description="A simple way to convert wmi data from wmi class objects into defaultdict.",
     install_requires=['impacket'],
 )
