35db6dfd870a173e03a47563b7503d734fcad3de
SilvioGiunge <alvolivre@live.com>
2018-01-22 15:26:57 -0200

    Removed install for license file and bumb version

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="wmi-query",
-    version="0.1.3",
+    version="0.1.4",
     license="BSD2CLAUSE",
     packages=find_packages(),
-    data_files=[('', ['LICENSE'])],
-    package_data={'': ['LICENSE']},
     keywords="wmi impacket",
     url="https://github.com/kanazux/wmi-query",
     author='Silvio AS a.k.a kanazuchi',
