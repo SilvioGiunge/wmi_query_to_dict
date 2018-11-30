5b9be5889a5b314423eb60e9625f1fe037e01ea0
m0ellemeister <m0ellemeister@users.noreply.github.com>
2018-11-30 11:07:44 +0100

    Corrected import in application
    I corrected the way I import wmi-quey in my Application, therefor I corrected the import for wmi_conn in wmi_query

--- wmi_query/wmi_query.py
+++ wmi_query/wmi_query.py
@@ -3,7 +3,7 @@
 """The class wmi_query returns a defaultdict with objects of specific class."""
 
 
-import wmi_query.wmi_conn as wmi_conn
+import wmi_conn
 from collections import defaultdict
 from datetime import datetime, timedelta
 
